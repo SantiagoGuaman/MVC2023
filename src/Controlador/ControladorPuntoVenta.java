9c2f78a0c89d08d2147ac1adce98dc2b7e21468b
Santiago G. <santiagogcan@gmail.com>
2023-01-11 22:34:32 -0500

    Añadido control de dialog (temporal)

--- src/Controlador/ControladorPuntoVenta.java
+++ src/Controlador/ControladorPuntoVenta.java
@@ -12,16 +12,23 @@ public class ControladorPuntoVenta {
     
     public void IniciaControl() {
         vpv.getBtnagregar().addActionListener(l -> AbreDialogo(1));
+        vpv.getBtncambiar().addActionListener(l -> AbreDialogo(2));
     }
     
     private void AbreDialogo(int op) {
         String title = null;
         if (op == 1) {
             title = "PRODUCTOS";
+            vpv.getDjgproductos().setLocationRelativeTo(vpv);
+            vpv.getDjgproductos().setSize(620, 350);
+            vpv.getDjgproductos().setTitle(title);
+            vpv.getDjgproductos().setVisible(true);
+        } else if(op == 2) {
+            title = "CLIENTES";
+            vpv.getDjgclientes().setLocationRelativeTo(vpv);
+            vpv.getDjgclientes().setSize(620, 350);
+            vpv.getDjgclientes().setTitle(title);
+            vpv.getDjgclientes().setVisible(true);
         }
-        vpv.getDjgproductos().setLocationRelativeTo(vpv);
-        vpv.getDjgproductos().setSize(620, 350);
-        vpv.getDjgproductos().setTitle(title);
-        vpv.getDjgproductos().setVisible(true);
     }
 }
