00bf01034cc7aa8e7875a75269f546e8584443ea
Santiago G. <santiagogcan@gmail.com>
2023-03-09 00:02:59 -0500

    Minor Changes

--- src/Controlador/ControlPersona.java
+++ src/Controlador/ControlPersona.java
@@ -26,7 +26,10 @@ import net.sf.jasperreports.engine.JRSubreport;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JRDesignSubreport;
+import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 public class ControlPersona {
